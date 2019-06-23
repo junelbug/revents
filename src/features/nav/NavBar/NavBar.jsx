8b3939415aaf0a5e97d0c7fa9fc6ec2fccfbdb12
junelbug <juneleigh@gmail.com>
2019-06-23 22:55:12 +0900

    Added Signin and Signout Menu DIfferentiation

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
 import { NavLink, Link } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
 
 class NavBar extends Component {
+  state = {
+    authenticated: true
+  };
   render() {
+    const { authenticated } = this.state;
     return (
       <Menu inverted fixed="top">
         <Container>
@@ -24,15 +30,7 @@ class NavBar extends Component {
               content="Create Event"
             />
           </Menu.Item>
-          <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button
-              basic
-              inverted
-              content="Sign Out"
-              style={{ marginLeft: "0.5em" }}
-            />
-          </Menu.Item>
+          {authenticated ? <SignedInMenu /> : <SignedOutMenu />}
         </Container>
       </Menu>
     );
