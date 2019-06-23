2fe7da6825ecc4b49923f40777a863e9aa2caa5e
junelbug <juneleigh@gmail.com>
2019-06-23 12:55:21 +0900

    End of Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Re-vents</h1>
+        <Fragment>
+          <NavBar/>
+          <Container className="main">            
+            <EventDashboard/>
+          </Container>
+
+        </Fragment>
+        
+        
       </div>
     );
   }
