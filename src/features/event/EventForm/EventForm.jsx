5dc00d860dc3e11ae1aea79c0f18c37da15c2e77
junelbug <juneleigh@gmail.com>
2019-06-27 18:58:48 +0900

    Error with EventForm

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,117 +1,112 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Segment, Form, Button } from "semantic-ui-react";
-import { createEvent, updateEvent } from "../eventActions";
-import cuid from "cuid";
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { Form, Segment, Button } from 'semantic-ui-react';
+import {createEvent, updateEvent} from '../eventActions';
+import cuid from 'cuid';
+import {reduxForm, Field} from 'redux-form'
+import TextInput from '../../../app/common/form/TextInput';
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
 
   let event = {
-    title: "",
-    date: "",
-    city: "",
-    venue: "",
-    hostedBy: ""
-  };
+    title: '',
+    date: '',
+    city: '',
+    venue: '',
+    hostedBy: ''
+  }
 
   if (eventId && state.events.length > 0) {
-    event = state.events.filter(event => event.id === eventId)[0];
+    event = state.events.filter(event => event.id === eventId)[0]
   }
 
   return {
     event
-  };
-};
+  }
+}
 
 const actions = {
   createEvent,
   updateEvent
-};
+}
 
 class EventForm extends Component {
-  state = { 
-    event: Object.assign({}, this.props.event) 
+  state = {
+    event: Object.assign({}, this.props.event)
   };
 
-  handleFormSubmit = evt => {
+  handleFormSubmit = () => {
     if (this.state.event.id) {
-      this.props.updateEvent(this.state.event);
-      this.props.history.goBack();
+      this.props.updateEvent(this.state.event)
+      this.props.history.goBack()
     } else {
       const newEvent = {
         ...this.state.event,
         id: cuid(),
-        hostPhotoURL: '/assests/user.png'
+        hostPhotoURL: '/assets/user.png'
       }
       this.props.createEvent(newEvent);
       this.props.history.push('/events')
     }
   };
 
-  handleInputChange = (evt) => {
-    const newEvent= this.state.event;
+  handleInputChange = (evt)=> {
+    const newEvent = this.state.event;
     newEvent[evt.target.name] = evt.target.value;
     this.setState({
       event: newEvent
     });
-  };
+  }
+
 
   render() {
     const { title, date, city, venue, hostedBy } = this.state.event;
     return (
       <Segment>
-        <Form onSubmit={this.handleFormSubmit} autoComplete="off">
-          <Form.Field>
-            <label>Event Title</label>
-            <input
-              name="title"
-              onChange={this.handleInputChange}
-              value={title}
-              placeholder="Event Title"
-            />
-          </Form.Field>
+        <Form onSubmit={this.handleFormSubmit}>
+          <Field name='title' component={TextInput} placeholder='Event Title'/>
           <Form.Field>
             <label>Event Date</label>
             <input
-              name="date"
-              onChange={this.handleInputChange}
+              name='date'
               value={date}
-              type="date"
-              placeholder="Event Date"
+              onChange={this.handleInputChange}
+              type='date'
+              placeholder='Event Date'
             />
           </Form.Field>
           <Form.Field>
             <label>City</label>
             <input
-              name="city"
-              onChange={this.handleInputChange}
+              name='city'
               value={city}
-              placeholder="City event is taking place"
+              onChange={this.handleInputChange}
+              placeholder='City event is taking place'
             />
           </Form.Field>
           <Form.Field>
             <label>Venue</label>
             <input
-              name="venue"
-              onChange={this.handleInputChange}
+              name='venue'
               value={venue}
-              placeholder="Enter the Venue of the event"
+              onChange={this.handleInputChange}
+              placeholder='Enter the Venue of the event'
             />
           </Form.Field>
           <Form.Field>
             <label>Hosted By</label>
             <input
-              name="hostedBy"
-              onChange={this.handleInputChange}
+              name='hostedBy'
               value={hostedBy}
-              placeholder="Enter the name of person hosting"
+              onChange={this.handleInputChange}
+              placeholder='Enter the name of person hosting'
             />
           </Form.Field>
-          <Button positive type="submit">
+          <Button positive type='submit'>
             Submit
           </Button>
-          <Button type="button" onClick={this.props.history.goBack}>
+          <Button onClick={this.props.history.goBack} type='button'>
             Cancel
           </Button>
         </Form>
@@ -120,7 +115,4 @@ class EventForm extends Component {
   }
 }
 
-export default connect(
-  mapState,
-  actions
-)(EventForm);
+export default connect(mapState, actions)(reduxForm({from:'eventForm'})(EventForm));
